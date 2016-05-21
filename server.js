04f7af96fe5a5d778a3b469d84b576a883885008
Ken <islandbodybuilding@gmail.com>
2016-05-21 07:22:54 +0000

    Added POST for todos

--- server.js
+++ server.js
@@ -2,26 +2,18 @@ var express = require("express");
 
 var app = express();
 var PORT = process.env.PORT;
-var todos = [
-        {
-            id: 1,
-            description:"Meet mom for lunch",
-            completed: false
-        },
-        
-        {
-            id:2,
-            description:"Go to the market",
-            completed: false
-        },
-        
-        {
-            id:3,
-            description:"Feed the cat",
-            completed:true
-        }
-    ];
+var bodyParser = require("body-parser");
 
+var todos = [];
+var todoNextId =1;
+
+app.use(bodyParser.json());
+
+//ROUTES
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+//Root route
 app.get("/",function(req,res){
     res.send("Todo API Root");
 });
@@ -54,7 +46,23 @@ app.get("/todos/:id",function(req,res){
 
 
 
-
+//POST request /todos
+app.post("/todos",function(req,res){
+    var body = req.body;
+    //add id field
+    body.id = todoNextId++;
+    //push body into array
+    // todos.push(body.description);
+    // todos.push(body.completed);
+    // todos.push(body.id);
+    
+    todos.push(body);
+    
+    console.log("description " + body.description );
+    console.log("completed " + body.completed);
+    console.log(todoNextId);
+    res.json(body);
+})
 
 
 
