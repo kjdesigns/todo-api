6b36cceb6f3e1be928e1a072f1659711cd7230f1
Kenneth Banico <islandbodybuilding@gmail.com>
2016-05-25 21:28:06 +1000

    fixed typos again

--- db.js
+++ db.js
@@ -1,30 +1,21 @@
-var Sequelize = require("sequelize");
-var env = process.env.NODE_ENV || "development";
+var Sequelize = require('sequelize');
+var env = process.env.NODE_ENV || 'development';
 var sequelize;
 
-if(env==="production"){
-<<<<<<< HEAD
-	sequelize = new Sequelize(process.env.DATABASE_URL,{
-=======
-<<<<<<< HEAD
-	sequelize = new Sequelize(process.env.DATABASE_URL,{
-=======
-	sequelize = new Sequelize(process.env.DATABAE_URL,{
->>>>>>> d4097dd1b570f84368123d67db1500492e592082
->>>>>>> 4655c16596bce3a665e9240ad25c2691980a2a90
-		dialect:"postgress"
+if (env === 'production') {
+	sequelize = new Sequelize(process.env.DATABASE_URL, {
+		dialect: 'postgres'
 	});
-}else{
-	sequelize = new Sequelize(undefined,undefined,undefined,{
-	    "dialect":"sqlite",
-	    "storage":__dirname + "/data/dev-todo-api.sqlite"
+} else {
+	sequelize = new Sequelize(undefined, undefined, undefined, {
+		'dialect': 'sqlite',
+		'storage': __dirname + '/data/dev-todo-api.sqlite'
 	});
 }
 
-
 var db = {};
 
-db.todo = sequelize.import(__dirname+"/models/todo.js");
+db.todo = sequelize.import(__dirname + '/models/todo.js');
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
